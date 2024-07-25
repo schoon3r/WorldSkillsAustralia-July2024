9f548ffbeba7c12591b8dcfca1f4eea79eacb15e
Jim Labilles <jim@labilles.com.au>
2024-07-25 14:15:51 +0930

    edit apply and destroy

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,24 +11,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-# # S3 Bucket for Terraform State
-# resource "aws_s3_bucket" "tf_state_bucket" {
-#   bucket = var.s3_bucket_name
-# }
-
-# # DynamoDB Table for Terraform State Locking
-# resource "aws_dynamodb_table" "tf_locks" {
-#   name           = var.dynamodb_table_name
-#   billing_mode   = "PAY_PER_REQUEST"
-#   hash_key       = "LockID"
-
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
-
-# Terraform Backend Configuration
 terraform {
   backend "s3" {
     bucket         = "tf-state-12der3"
